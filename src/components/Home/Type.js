069cf937697cec9a01c2048a4675d2af7260e667
Dushyant Mahajan <dushyant.mahajan@raga.ai>
2024-10-03 18:03:50 -0400

    Update Home/Type.js and ProjectCards.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,8 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
+          "Data Scientist",
           "Software Developer",
-          "Freelancer",
           "MERN Stack Developer",
           "Open Source Contributor",
         ],
