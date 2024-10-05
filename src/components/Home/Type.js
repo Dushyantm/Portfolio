0df7526ca691a4773b1c554de076c5f872e94dae
Dushyant Mahajan <dushyant.mahajan@raga.ai>
2024-10-04 20:41:46 -0400

    Update Home/Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,7 +8,6 @@ function Type() {
         strings: [
           "Data Scientist",
           "Software Developer",
-          "MERN Stack Developer",
           "Open Source Contributor",
         ],
         autoStart: true,
